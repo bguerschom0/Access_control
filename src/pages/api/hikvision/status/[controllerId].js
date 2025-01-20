f8af455dc1b6be4e3d727e67099505ebb408507b
bguerschom0 <bguerschom0@gmail.com>
2025-01-20 18:05:48 +0200

    Update [controllerId].js

--- src/pages/api/hikvision/status/[controllerId].js
+++ src/pages/api/hikvision/status/[controllerId].js
@@ -1,4 +1,4 @@
-import { createHikvisionClient, handleHikvisionError } from '../../utils/hikvision';
+import axios from 'axios';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
@@ -6,24 +6,28 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { controllerId } = req.query;
-    // In production, you'd fetch controller details from your database
-    const controllerData = req.headers;
-    
-    const hikvisionClient = createHikvisionClient(controllerData);
-    const response = await hikvisionClient.get('/ISAPI/System/status');
+    const { ip_address, port, username, password } = req.headers;
+
+    const instance = axios.create({
+      baseURL: `http://${ip_address}:${port}`,
+      headers: {
+        Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
+        'Content-Type': 'application/json'
+      },
+      timeout: 5000
+    });
+
+    const response = await instance.get('/ISAPI/System/status');
 
     return res.status(200).json({
       success: true,
-      status: {
-        cpuUsage: response.data.CPUUsage,
-        memoryUsage: response.data.MemoryUsage,
-        deviceTime: response.data.DeviceTime,
-        deviceName: response.data.DeviceName
-      }
+      status: response.data
     });
   } catch (error) {
     console.error('Status check error:', error);
-    return res.status(500).json(handleHikvisionError(error));
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
   }
 }
